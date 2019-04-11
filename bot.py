a78695a46ffddd8a95fde0f2275fb1dd22273fd4
senateurritchie <zakeszako@yahoo.fr>
2019-04-11 06:08:47 +0000

    initial commit

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 
 from typing import List 
+from bs4 import BeautifulSoup
+from lxml import etree
 
 class AddressItem:
 	"""docstring for AddressItem"""
@@ -29,22 +31,43 @@ class Person:
 		self.website = None 
 		self.address:Address = None
 
-class Bot:
+class ChatBot:
 	def __init__(self):
 		self.name = None
 		self.version = None
 		self.Person:Author = None
+		self.soup:BeautifulSoup = None
 
-	def __repr__(self):
-		return "Bot name<{name}>, version<{version}>".format(name=self.name, version=self.version)
+	def __repr__(self) -> str:
+		return "ChatBot name<{name}>, version<{version}>".format(name=self.name, version=self.version)
+
+	def run(self):
+		xml_content:str = None;
+		xsd_root:str = None;
+
+		# with open("data.xml") as f:
+		# 	xml_root = f.read()
+
+		# with open("schema.xsd") as f:
+		# 	xsd_root = f.read()
+
+		# print(self.soup)
+
+		xml_root = etree.parse("data.xml")
+		xsd_root = etree.parse("schema.xsd")
+		schema = etree.XMLSchema(xsd_root)
+
+		print(schema)
+		schema.validate(xml_root)
 
 
 
 if __name__ == "__main__":
-	bot:Bot = Bot()
+	bot:ChatBot = ChatBot()
 	bot.name = "Khallil"
 	bot.version = "2.3"
 	print(bot)
+	bot.run()
 
 
 
